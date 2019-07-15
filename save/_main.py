0d3f7f3d2605e5f20e3709bb80a749f4786a43e5
Adam Dodge <adam.dodge@colorado.edu>
2019-07-15 08:58:26 -0600

    Debugging slow save

--- save/_main.py
+++ save/_main.py
@@ -61,17 +61,23 @@ def main():
                                         .where(lidar.columns.station_id == s[0])
                                         .order_by(lidar.columns.unix_time)
                                         ).fetchmany(1000000)
+        print('a')
         lidar_ids = False
         while len(lidar_data) > 0:
+            print('b')
             save_lidar(lidar_data, args.directory, s[1])
+            print('c')
             lidar_ids = [i[0] for i in lidar_data]
+            print('d')
             connection.execute(db.delete(lidar).where(lidar.columns.id.in_(lidar_ids)))
+            print('e')
             lidar_data = connection.execute(db.select([lidar])
                                             .where(lidar.columns.unix_time < unix_today)
                                             .where(lidar.columns.unix_time > unix_yesterday)
                                             .where(lidar.columns.station_id == s[0])
                                             .order_by(lidar.columns.unix_time)
                                             ).fetchmany(1000000)
+            print('f')
         if lidar_ids is not False:
             print("LiDAR Data saved for " + s[1])
 
