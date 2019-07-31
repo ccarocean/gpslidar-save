a73a7b1eb4fab5073dc6d4405e7a5b978f4eb446
Adam Dodge <adam.dodge@colorado.edu>
2019-07-31 09:21:23 -0600

    Debugging plot

--- save/plot.py
+++ save/plot.py
@@ -81,8 +81,8 @@ def plot_gps(longname, save_dir, data_dir):
     altstd, altmean = np.std(alt), np.mean(alt)
     print(latstd, latmean)
 
-    indlat = np.where(np.abs(lat - latmean) < 0.035*latstd)
-    indlon = np.where(np.abs(lon - lonmean) < 0.035*lonstd)
+    indlat = np.where(np.abs(lat - latmean) < 0.045*latstd)
+    indlon = np.where(np.abs(lon - lonmean) < 0.045*lonstd)
     indalt = np.where(np.abs(alt - altmean) < 3*altstd)
     lat_no = lat[indlat]
     lon_no = lon[indlon]
